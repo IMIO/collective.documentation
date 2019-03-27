cf968606c3a6dd8bb434e32af0c840fb7085059c
Piret Valentin <valentin.piret@affinitic.be>
2019-03-27 13:23:00 +0100

    Blacked

--- src/collective/documentation/content/documentation_file.py
+++ src/collective/documentation/content/documentation_file.py
@@ -10,20 +10,12 @@ from collective.documentation import _
 
 class IDocumentationFile(model.Schema):
     """IDocumentationFile"""
-    source_title = schema.TextLine(
-        title=_(u"Source title"),
-        required=False,
-    )
 
-    source = schema.URI(
-        title=_(u"Source"),
-        required=False,
-    )
+    source_title = schema.TextLine(title=_(u"Source title"), required=False)
 
-    file = NamedBlobFile(
-        title=_(u"File"),
-        required=True,
-    )
+    source = schema.URI(title=_(u"Source"), required=False)
+
+    file = NamedBlobFile(title=_(u"File"), required=True)
 
 
 class DocumentationFile(File):
